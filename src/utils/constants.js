e6772067b677f8a94fbe386ec7b05623fb5df29b
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2024-04-06 13:37:42 +0200

    Add constants.js file with status codes, recommended books limit, provider types, and URLs

---
+++ src/utils/constants.js
@@ -0,0 +1,21 @@
+export const STATUS_CODES = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+export const RECOMMENDED_BOOKS_LIMIT = 5;
+
+export const PROVIDER_TYPES = {
+  EMAIL: 'email',
+  SMS: 'sms',
+};
+
+export const URLS = {
+  EMAIL_PROVIDER_URL:
+    'https://run.mocky.io/v3/8eb88272-d769-417c-8c5c-159bb023ec0a',
+  SMS_PROVIDER_URL:
+    'https://run.mocky.io/v3/268d1ff4-f710-4aad-b455-a401966af719',
+};
