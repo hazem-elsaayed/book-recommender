0529587927348407aa97843b8327e723fc643dfc
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2024-04-06 23:11:06 +0200

    Refactor sendNotification method in NotificationService to use a template message

--- src/services/notification.service.js
+++ src/services/notification.service.js
@@ -5,18 +5,26 @@ export class NotificationService {
     this.userService = userService;
   }
 
-  async sendNotification({ userId }, message) {
+  async sendNotification(user) {
     try {
-      const user = await this.userService.getUserById(userId);
       const notificationProvider = NotificationProviderFactory.getProvider(
         process.env.NOTIFICATION_PROVIDER
       );
-      await notificationProvider.sendNotification(user, message);
+      await notificationProvider.sendNotification(user, this.getMessage(user));
       return { message: `Notification sent to ${user.name}`, success: true };
     } catch (error) {
       console.error(error);
       // improvement: send to a message broker for retry
       return { error: 'Failed to send notification', success: false };
     }
+  }
+
+  getMessage({ name }) {
+    return `Dear ${name}, 
+
+    Thank you for your submission! We appreciate your contribution and value your participation. Your support means a lot to us.
+
+    Best regards,
+    The Book Recommender Team`;
   }
 }
