8f84eb865f8f40411008bd93c16155c809168bd7
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2024-04-06 14:13:41 +0200

    Add swagger configuration file

---
+++ src/config/swagger.js
@@ -0,0 +1,14 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['src/routes.js'],
+};
+
+export const swaggerSpecs = swaggerJsdoc(options);
