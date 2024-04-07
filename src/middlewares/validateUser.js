00efcf5de490c4ba4e9029f4239de88490024630
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2024-04-07 22:17:14 +0200

    Refactor validateUser middleware to accept userRepository as a parameter

--- src/middlewares/validateUser.js
+++ src/middlewares/validateUser.js
@@ -1,9 +1,7 @@
-import { UserRepository } from '../repositories/user.repository.js';
 import { STATUS_CODES } from '../utils/constants.js';
 
-export const validateUser = async (req, res, next) => {
+export const validateUser = (userRepository) => async (req, res, next) => {
   const { user_id } = req.body;
-  const userRepository = new UserRepository();
   const user = await userRepository.getUserById(user_id);
   if (!user) {
     return res
